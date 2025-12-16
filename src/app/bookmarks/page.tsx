94cb9ca95727c2140f22b3c13aab8d235dd952c3
qiang.zeng <qiang.zeng@iglooinsure.com>
2025-12-16 09:59:17 +0800

    feat: Bookmarks

--- src/app/bookmarks/page.tsx
+++ src/app/bookmarks/page.tsx
@@ -1,74 +1,74 @@
-import { BookmarkCard } from "@/components/bookmark/BookmarkCard";
-import { SimpleLayout } from "@/components/layout/SimpleLayout";
+import { BookmarkCard } from '@/components/bookmark/BookmarkCard'
+import { SimpleLayout } from '@/components/layout/SimpleLayout'
 import {
-  blogs,
-	bookmarkHeadLine,
-	bookmarkIntro,
-	developerTools,
-	navigationStations,
-	uiComponents,
-} from "@/config/bookmarks";
+  articles,
+  bookmarkHeadLine,
+  bookmarkIntro,
+  developerTools,
+  navigationStations,
+  uiComponents,
+} from '@/config/bookmarks'
 
 export default function Bookmarks() {
-	return (
-		<SimpleLayout title={bookmarkHeadLine} intro={bookmarkIntro}>
-			{/*  UI Component */}
-			<div className="mx-auto my-4 flex max-w-xl flex-col gap-6 border-t border-muted py-8 lg:max-w-none">
-				<h2 className="mb-4 flex flex-row items-center justify-start gap-2 text-xl font-semibold tracking-tight opacity-80 md:text-3xl">
-					UI Component
-				</h2>
-				<ul
-					role="list"
-					className="grid grid-cols-1 gap-x-8 gap-y-12 sm:grid-cols-2 md:grid-cols-3"
-				>
-					{uiComponents.map((bookmark) => (
-						<BookmarkCard key={bookmark.name} bookmark={bookmark} />
-					))}
-				</ul>
-			</div>
+  return (
+    <SimpleLayout title={bookmarkHeadLine} intro={bookmarkIntro}>
+      {/*  UI Component */}
+      <div className="mx-auto my-4 flex max-w-xl flex-col gap-6 border-t border-muted py-8 lg:max-w-none">
+        <h2 className="mb-4 flex flex-row items-center justify-start gap-2 text-xl font-semibold tracking-tight opacity-80 md:text-3xl">
+          UI Component
+        </h2>
+        <ul
+          role="list"
+          className="grid grid-cols-1 gap-x-8 gap-y-12 sm:grid-cols-2 md:grid-cols-3"
+        >
+          {uiComponents.map((bookmark) => (
+            <BookmarkCard key={bookmark.name} bookmark={bookmark} />
+          ))}
+        </ul>
+      </div>
 
-			{/* Navigation Station */}
-			<div className="mx-auto my-4 flex max-w-xl flex-col gap-6 border-t border-muted py-8 lg:max-w-none">
-				<h2 className="mb-4 flex flex-row items-center justify-start gap-2 text-xl font-semibold tracking-tight opacity-80 md:text-3xl">
-					Navigation Station
-				</h2>
-				<ul
-					role="list"
-					className="grid grid-cols-1 gap-x-8 gap-y-12 sm:grid-cols-2 md:grid-cols-3"
-				>
-					{navigationStations.map((bookmark) => (
-						<BookmarkCard key={bookmark.name} bookmark={bookmark} />
-					))}
-				</ul>
-			</div>
-			{/* Developer Tools */}
-			<div className="mx-auto my-4 flex max-w-xl flex-col gap-6 border-t border-muted py-8 lg:max-w-none">
-				<h2 className="mb-4 flex flex-row items-center justify-start gap-2 text-xl font-semibold tracking-tight opacity-80 md:text-3xl">
-					Developer Tool
-				</h2>
-				<ul
-					role="list"
-					className="grid grid-cols-1 gap-x-8 gap-y-12 sm:grid-cols-2 md:grid-cols-3"
-				>
-					{developerTools.map((bookmark) => (
-						<BookmarkCard key={bookmark.name} bookmark={bookmark} />
-					))}
-				</ul>
-			</div>
-			{/* Blogs */}
-			<div className="mx-auto my-4 flex max-w-xl flex-col gap-6 border-t border-muted py-8 lg:max-w-none">
-				<h2 className="mb-4 flex flex-row items-center justify-start gap-2 text-xl font-semibold tracking-tight opacity-80 md:text-3xl">
-					Blogs
-				</h2>
-				<ul
-					role="list"
-					className="grid grid-cols-1 gap-x-8 gap-y-12 sm:grid-cols-2 md:grid-cols-3"
-				>
-					{blogs.map((bookmark) => (
-						<BookmarkCard key={bookmark.name} bookmark={bookmark} />
-					))}
-				</ul>
-			</div>
-		</SimpleLayout>
-	);
+      {/* Navigation Station */}
+      <div className="mx-auto my-4 flex max-w-xl flex-col gap-6 border-t border-muted py-8 lg:max-w-none">
+        <h2 className="mb-4 flex flex-row items-center justify-start gap-2 text-xl font-semibold tracking-tight opacity-80 md:text-3xl">
+          Navigation Station
+        </h2>
+        <ul
+          role="list"
+          className="grid grid-cols-1 gap-x-8 gap-y-12 sm:grid-cols-2 md:grid-cols-3"
+        >
+          {navigationStations.map((bookmark) => (
+            <BookmarkCard key={bookmark.name} bookmark={bookmark} />
+          ))}
+        </ul>
+      </div>
+      {/* Developer Tools */}
+      <div className="mx-auto my-4 flex max-w-xl flex-col gap-6 border-t border-muted py-8 lg:max-w-none">
+        <h2 className="mb-4 flex flex-row items-center justify-start gap-2 text-xl font-semibold tracking-tight opacity-80 md:text-3xl">
+          Developer Tool
+        </h2>
+        <ul
+          role="list"
+          className="grid grid-cols-1 gap-x-8 gap-y-12 sm:grid-cols-2 md:grid-cols-3"
+        >
+          {developerTools.map((bookmark) => (
+            <BookmarkCard key={bookmark.name} bookmark={bookmark} />
+          ))}
+        </ul>
+      </div>
+      {/* Articles */}
+      <div className="mx-auto my-4 flex max-w-xl flex-col gap-6 border-t border-muted py-8 lg:max-w-none">
+        <h2 className="mb-4 flex flex-row items-center justify-start gap-2 text-xl font-semibold tracking-tight opacity-80 md:text-3xl">
+          Articles
+        </h2>
+        <ul
+          role="list"
+          className="grid grid-cols-1 gap-x-8 gap-y-12 sm:grid-cols-2 md:grid-cols-3"
+        >
+          {articles.map((bookmark) => (
+            <BookmarkCard key={bookmark.name} bookmark={bookmark} />
+          ))}
+        </ul>
+      </div>
+    </SimpleLayout>
+  )
 }
--- src/config/bookmarks.ts
+++ src/config/bookmarks.ts
@@ -1,112 +1,127 @@
 // bookmarks
 export const bookmarkHeadLine = "What I'm using"
-export const bookmarkIntro = "I often come across some tools that can assist us in certain ways and improve efficiency. Here is my collection of tool websites."
-
+export const bookmarkIntro =
+  'I often come across some tools that can assist us in certain ways and improve efficiency. Here is my collection of tool websites.'
 
 export type BookmarkItemType = {
-  name: string;
-  logo?: string;
-  url: string;
-  description: string;
+  name: string
+  logo?: string
+  url: string
+  description: string
 }
 
 // UI Components
 export const uiComponents: Array<BookmarkItemType> = [
   {
-    name: "Origin UI",
-    logo: "https://originui.com/icon.svg",
-    url: "https://originui.com",
-    description: "An open-source collection of copy-and-paste components for quickly build application UIs."
+    name: 'Origin UI',
+    logo: 'https://originui.com/icon.svg',
+    url: 'https://originui.com',
+    description:
+      'An open-source collection of copy-and-paste components for quickly build application UIs.',
   },
   {
-    name: "Aceternity UI",
-    logo: "https://ui.aceternity.com/logo.png",
-    url: "https://ui.aceternity.com",
-    description: "Copy paste the most trending components and use them in your websites without having to worry about styling and animations."
+    name: 'Aceternity UI',
+    logo: 'https://ui.aceternity.com/logo.png',
+    url: 'https://ui.aceternity.com',
+    description:
+      'Copy paste the most trending components and use them in your websites without having to worry about styling and animations.',
   },
   {
-    name: "Magic UI",
-    logo: "https://magicui.design/favicon.ico",
-    url: "https://magicui.design",
-    description: "150+ free and open-source animated components and effects built with React, Typescript, Tailwind CSS, and Motion. Perfect companion for shadcn / ui."
+    name: 'Magic UI',
+    logo: 'https://magicui.design/favicon.ico',
+    url: 'https://magicui.design',
+    description:
+      '150+ free and open-source animated components and effects built with React, Typescript, Tailwind CSS, and Motion. Perfect companion for shadcn / ui.',
   },
   {
-    name: "React Bits",
-    logo: "https://reactbits.dev/favicon.ico",
-    url: "https://reactbits.dev/",
-    description: "Animated React components for creative developers"
+    name: 'React Bits',
+    logo: 'https://reactbits.dev/favicon.ico',
+    url: 'https://reactbits.dev/',
+    description: 'Animated React components for creative developers',
   },
   {
-    name: "21st.dev",
-    logo: "https://21st.dev//brand/21st-logo-dark.svg",
-    url: "https://21st.dev",
-    description: "Discover and share video components. Ready-to-use React Tailwind components inspired by shadcn/ui."
-  }
+    name: '21st.dev',
+    logo: 'https://21st.dev//brand/21st-logo-dark.svg',
+    url: 'https://21st.dev',
+    description:
+      'Discover and share video components. Ready-to-use React Tailwind components inspired by shadcn/ui.',
+  },
 ]
-
 
 // Navigation Station
 export const navigationStations: Array<BookmarkItemType> = [
   {
-    name: "IndieHub Tools",
-    logo: "https://indiehub.best/logo.png",
-    url: "https://indiehub.best/tool",
-    description: "Discover 400+ best tools for efficient indie hacking, from Domain, Database, Design, to Template, CMS, Marketing, etc."
+    name: 'IndieHub Tools',
+    logo: 'https://indiehub.best/logo.png',
+    url: 'https://indiehub.best/tool',
+    description:
+      'Discover 400+ best tools for efficient indie hacking, from Domain, Database, Design, to Template, CMS, Marketing, etc.',
   },
   {
-    name: "FMHY",
-    url: "https://fmhy.net",
-    description: "The largest collection of free stuff on the internet!"
-  }
+    name: 'FMHY',
+    url: 'https://fmhy.net',
+    description: 'The largest collection of free stuff on the internet!',
+  },
 ]
 
 // Developer Tools
 export const developerTools: Array<BookmarkItemType> = [
   {
-    name: "grep.app",
-    logo: "https://grep.app/icon.png",
-    url: "https://grep.app",
-    description: "Effortlessly search for code, files, and paths across a million GitHub repositories."
+    name: 'grep.app',
+    logo: 'https://grep.app/icon.png',
+    url: 'https://grep.app',
+    description:
+      'Effortlessly search for code, files, and paths across a million GitHub repositories.',
   },
   {
-    name: "Map of GitHub",
-    logo: "https://anvaka.github.io/map-of-github/favicon-32x32.png",
-    url: "https://anvaka.github.io/map-of-github/",
-    description: "This is a map of 690,000+ GitHub projects. Each dot is a project. Dots are close to each other if they have a lot of common stargazers."
+    name: 'Map of GitHub',
+    logo: 'https://anvaka.github.io/map-of-github/favicon-32x32.png',
+    url: 'https://anvaka.github.io/map-of-github/',
+    description:
+      'This is a map of 690,000+ GitHub projects. Each dot is a project. Dots are close to each other if they have a lot of common stargazers.',
   },
   {
-    name: "npm compare",
-    url: "https://npm-compare.com",
-    description: "Search for similar packages and compare them in terms of popularity, features, and discover popular alternatives.",
+    name: 'npm compare',
+    url: 'https://npm-compare.com',
+    description:
+      'Search for similar packages and compare them in terms of popularity, features, and discover popular alternatives.',
   },
   {
-    name: "DeepWiki",
-    url: "https://deepwiki.org",
-    description: "DeepWiki | AI documentation you can talk to, for every repo"
+    name: 'DeepWiki',
+    url: 'https://deepwiki.org',
+    description: 'DeepWiki | AI documentation you can talk to, for every repo',
   },
   {
-    name: "Zread",
-    url: "https://zread.ai/",
-    description: "Explore Deep Research in Zread, the AI code wiki with multilingual guides, architecture insights, and community buzz, helping you dive into any GitHub repo in depth."
+    name: 'Zread',
+    url: 'https://zread.ai/',
+    description:
+      'Explore Deep Research in Zread, the AI code wiki with multilingual guides, architecture insights, and community buzz, helping you dive into any GitHub repo in depth.',
   },
   {
-    name: "Code Wiki",
-    url: "https://codewiki.google",
-    description: "A new perspective on development for the agentic era. Gemini-generated documentation, always up-to-date."
-  }
+    name: 'Code Wiki',
+    url: 'https://codewiki.google',
+    description:
+      'A new perspective on development for the agentic era. Gemini-generated documentation, always up-to-date.',
+  },
 ]
 
-// Blogs
-export const blogs: Array<BookmarkItemType> = [
+// Articles
+export const articles: Array<BookmarkItemType> = [
   {
-    name: "宝玉的分享",
-    url: "https://baoyu.io/",
-    description: "宝玉的分享，主要在大语言模型、Prompt Engineering、软件工程、工程管理、前端等领域。"
+    name: '宝玉的分享',
+    url: 'https://baoyu.io/',
+    description:
+      '宝玉的分享，主要在大语言模型、Prompt Engineering、软件工程、工程管理、前端等领域。',
   },
   {
-    name: "CSS-Tricks",
-    url: "https://css-tricks.com/",
-    description: "CSS-Tricks - A Website About Making Websites"
-  }
+    name: 'CSS-Tricks',
+    url: 'https://css-tricks.com/',
+    description: 'CSS-Tricks - A Website About Making Websites',
+  },
+  {
+    name: 'BestBlogs.dev',
+    url: 'https://www.bestblogs.dev/',
+    logo: 'https://www.bestblogs.dev/favicon.ico',
+    description: '遇见更好的技术阅读',
+  },
 ]
-
